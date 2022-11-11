b16e20812c8c1c28d0ab4fdf20849b9b5dfa584e
Carranya <k.gong82@gmail.com>
2022-11-11 07:18:42 +0100

    create menu functions

--- js/menu.js
+++ js/menu.js
@@ -1,16 +1,10 @@
-window.onload = function () {
-    const pages = ['home', 'websites', 'about'];
-    let firstContent = document.getElementById(pages[0]).innerHTML;
-    document.getElementById('contents').innerHTML = firstContent;
-
-    for (let i = 0; i < 3; i++) {
-        document.getElementById(pages[i]).addEventListener("click", function (e) { showPage(e); });
-    }
+window.onload = function(){
+    document.getElementById("navigation").addEventListener("click", (openWebsites));
+    
 }
 
-function showPage(e) {
-
-    let pickPage = e.target.value;
-    let contents = document.getElementById(pickPage).innerHTML;
-    document.getElementById('contents').innerHTML = contents;
+function openWebsites(){
+    // document.getElementById("main").style.transform = "translate(0, -25%)";
+    document.getElementById("idAbout").scrollIntoView();
 }
+
