9f9c5cea7bb136af798951edbbef5ae39e22c219
Carranya <k.gong82@gmail.com>
2022-11-11 07:18:49 +0100

    remove footer page

--- pages/footer.php
+++
@@ -1,9 +0,0 @@
-<div id='address'>
-            Karin Giang<br>
-            Schützenmattstrasse 7<br>
-            8302 Kloten<br>
-            Phone: +41 78 823 67 56<br>
-            <a href="mailto:karin.giang1982@gmail.com"><img src="img/icons/mail.png" width="20" height="20" alt="karin.giang1982@gmail.com"></img></a>
-            <a href="https://www.linkedin.com/in/karin-giang-0a6072247" target="_blank"><img src="img/icons/linkedin.png" width="20" height="20"></img></a>
-            <a href="https://github.com/Carranya" target="_blank"><img src="img/icons/github.png" width="20" height="20"></img></a>
-        </div>
