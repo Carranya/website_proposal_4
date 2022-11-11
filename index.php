625997e84db0ff189ae81bbac8734004384f134c
Carranya <k.gong82@gmail.com>
2022-11-11 07:18:26 +0100

    created new page

--- index.php
+++ index.php
@@ -9,26 +9,13 @@
     <script type="text/javascript" src="js/menu.js"></script>
 </head>
 <body>
-    <div id='displayNone'>
-        <div id='pageHome'>Home</div>
-        <div id='pageWebsites'><?php include "pages/websites.php" ?></div>
-        <div id='pageAbout'><?php include "pages/about.php" ?></div>
+    <div id="main">
+        <div id="navigation">menu </div>        
+        <div id="idHome" class="pageContent"><?php include "pages/home.php"; ?></div>
+        <div id="idWebsites" class="pageContent"><?php include "pages/websites.php"; ?></div>
+        <div id="idAbout" class="pageContent"><?php include "pages/about.php"; ?></div>
+        <div id="idContact" class="pageContent"><?php include "pages/contact.php"; ?></div>
+        <div id="footer">foot</div>
     </div>
-
-    <div id='header'><p>Website Proposal 4</p></div>
-
-    <div id='main'>
-        <div id='menu'>
-            <button id='home' class='classButton' value='pageHome'>Home</button>
-            <button id='websites' class='classButton'  value='pageWebsites'>Websites</button>
-            <button id='about' class='classButton'  value='pageAbout'>About</button>
-        </div>
-        <div id='centerContents'>
-            <div id='contents'></div>
-        </div>
-    </div>
-
-    <div id='footer'><?php include "pages/footer.php" ?></div>
 </body>
 </html>
-
