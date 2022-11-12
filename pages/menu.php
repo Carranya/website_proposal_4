3512f1db6a334a6590fb2b031c6906418d0fd9a8
Carranya <k.gong82@gmail.com>
2022-11-12 13:34:24 +0100

    cleaned up

--- pages/menu.php
+++
@@ -1,8 +0,0 @@
-<div class='centerPage'>
-    <ul id='menubar'>
-        <li id='home' class='menuButton'>Home</li>
-        <li id='websites' class='menuButton'>Webseiten</li>
-        <li id='about' class='menuButton'>Über mich</li>
-        <li id='contact' class='menuButton'>Kontakt</li>
-    </ul>
-</div>
--- pages/websites.php
+++ pages/websites.php
@@ -5,19 +5,12 @@
         Am Liebsten programmiere ich Webseiten.<br>
         Es ist auch mein Hobby, immer wieder neue Ideen für Webseiten zu entwickeln und diese auf dem Bildschirm zu bringen.<br>
         Alle Codes von meiner Webseiten Vorschlägen sind in meiner <a href="https://github.com/Carranya"><img src="img/icons/github.png" id="websitesLogo"></img></a>-Seite</b> einsehbar.</p>
-<<<<<<< HEAD
         <div class="centerPage">
             <div id="websitesContents">
                 <a href="projects/homepage_v1/index.php" target="_blank"><img src="img/websites/homepage_v1.jpg" class="websitesPic"></a>
                 <a href="projects/website_proposal_1/index.php" target="_blank"><img src="img/websites/website_proposal_1.jpg" class="websitesPic"></a>
                 <a href="projects/website_proposal_3/index.php" target="_blank"><img src="img/websites/website_proposal_3.jpg" class="websitesPic"></a>
             </div>
-=======
-        <div class="websitesContent">
-            <a href="projects/homepage_v1/index.php" target="_blank"><img src="img/websites/homepage_v1.jpg" class="websitesPic"></a>
-            <a href="projects/website_proposal_1/index.php" target="_blank"><img src="img/websites/website_proposal_1.jpg" class="websitesPic"></a>
-            <a href="projects/website_proposal_3/index.php" target="_blank"><img src="img/websites/website_proposal_3.jpg" class="websitesPic"></a>
->>>>>>> 186a99b56ab4f1770793ce823d9b4984131a001e
         </div>
     </div>
 </div>
