186a99b56ab4f1770793ce823d9b4984131a001e
Carranya <k.gong82@gmail.com>
2022-11-12 11:54:44 +0100

    new pages

--- pages/about.php
+++ pages/about.php
@@ -1,14 +1,24 @@
 <div class="centerPage">
     <div class="innerPage">
-        <h1>Website Proposal 2</h1>
-        <h2>Herzlich Willkommen</h2>
+        <h2>Über mich</h2>
+        <div class="centerPic"><img src="img/karin2.jpg" class="classPic" alt="Karin Giang" title="Karin Giang"></img></div>
         <p>
-        Diese Webseite dient als Vorschlag für ein Projekt.<br>
-        Es ist schlicht aufgebaut, aber die Menü-Buttons sowie die Navigationsbar sind mit Javascript gesteuert.<br><br>
-
-        Ursprünglich wollte die diese Seite mit Slideeffekt ausstatten.<br>
-        Stattdessen habe ich entschieden, etwas mehr Fancy einzubauen.
-        </p>
+            Guten Tag, ich bin Karin Giang.
+            <br><br>
+            Ich bin eine Programmiererin und Webentwicklerin.
+            <br><br>
+            Momentan bin ich auf der Suche nach einer Firma, die bereit ist, 
+            mir den Einstieg in die Informatik Branche zu ermöglichen.
+            Ich habe zwar bisher noch keine Ausbildung im Bereich Informatik absolviert, 
+            aber ich konnte privat durch Selbststudium das Programmieren von HTML, CSS, PHP, MySQL, Javascript etc. aneignen.
+            Näheres dazu finden Sie auf meiner Profil-Seite (Icon rechts unten).
+            <br><br>
+            In meiner Freizeit programmiere ich sehr gerne und lerne immer neue Techniken und Programmiersprachen kennen.
+            Bisher habe ich bereits kleine Webtools sowie auch Webpages programmiert.    
+            <br><br>
+            In offiziellen Dokumenten verwende ich noch den männlichen Namen Martin.
+            Jedoch in der Freizeit werden meine Freunde mich immer mehr als weiblich ansprechen und ansehen.
+            Momentan bin ich noch sehr beschäftigt mit der beruflichen Wandel. Sobald ich mit der neuen Beruf stabilisiert habe, werde ich mit der Hormontheraphie beginnen.</p>
         <p>Meine offizielle Webseite: <span id="officalHomepage"><a href="http://www.karingiang.ch" target="_blank">Hier klicken</span></a></p>
     </div>
 </div>
---
+++ pages/menu.php
@@ -0,0 +1,8 @@
+<div class='centerPage'>
+    <ul id='menubar'>
+        <li id='home' class='menuButton'>Home</li>
+        <li id='websites' class='menuButton'>Webseiten</li>
+        <li id='about' class='menuButton'>Über mich</li>
+        <li id='contact' class='menuButton'>Kontakt</li>
+    </ul>
+</div>
