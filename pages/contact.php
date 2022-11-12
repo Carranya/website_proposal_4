b1f00a37d5ea647dc02bc8286eddb736a823d401
Carranya <k.gong82@gmail.com>
2022-11-12 15:14:25 +0100

    new email

--- pages/contact.php
+++ pages/contact.php
@@ -9,7 +9,7 @@
         8302 Kloten<br></p>
         <p><b>Kontaktinformationen:</b><br>
         <img src="img/icons/phone.png" class="contactSmallIcons">: 078 / 823 67 56<br>
-        <img src="img/icons/mail.png" class="contactSmallIcons">: <u><a href="mailto:m.giang82@gmail.com">m.giang82@gmail.com</a></u><br><br>
+        <img src="img/icons/mail.png" class="contactSmallIcons">: <u><a href="mailto:karin.giang1982@gmail.com">karin.giang1982@gmail.com</a></u><br><br>
         <a href="https://www.linkedin.com/in/karin-giang-0a6072247" target="_blank"><img src="img/icons/linkedin.png" class="contactBigIcons"></a>
         <a href="https://github.com/Carranya" target="_blank"><img src="img/icons/github.png" class="contactBigIcons"></a>
         <p><b>Sonstige Informationen:</b><br>
